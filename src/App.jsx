b2d311dd01ea9076bc489dd5a87fdb61b5f23223
Juan Carlos Camacho H <130729402+camachocodecrafter@users.noreply.github.com>
2023-06-15 16:28:24 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import MyApp from "./Componentes/MyAPP"
+function App() {
+ 
+
+  return (
+    <> 
+       <MyApp/>
+    <h1>Hola!</h1>
+    <h2>Mundo</h2>
+    <h3>Cruel</h3>
+    </>
+  )
+}
+
+export default App
